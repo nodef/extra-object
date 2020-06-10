12b2207d00a9bcea60ed854eac8439143d02553b
Subhajit Sahu <wolfram77@gmail.com>
2020-06-10 22:18:31 +0530

    :watermelon: cool fruit +isEqual

--- src/isEqual.ts
+++ src/isEqual.ts
@@ -1,4 +1,7 @@
-function isEqual(x: object, y: object) {
+import compare from './compare';
+import type {compareFn, mapFn} from './_types';
 
+function isEqual(x: object, y: object, fc: compareFn=null, fm: mapFn=null): boolean {
+  return compare(x, y, fc, fm)===0;
 }
 export default isEqual;
