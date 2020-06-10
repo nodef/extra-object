36ce4155ddf22ddd237f2b820373b7c0573e6d0d
Subhajit Sahu <wolfram77@gmail.com>
2020-06-10 20:56:19 +0530

    :watermelon: cool fruit -isUnique

--- src/isUnique.ts
+++
@@ -1,8 +0,0 @@
-import {isUnique as iterableIsUnique} from 'extra-iterable';
-import type {compareFn, mapFn} from './_types';
-
-function isUnique(x: object, fc: compareFn=null, fm: mapFn=null): boolean {
-  if(fc || fm) return iterableIsUnique(Object.keys(x), fc, fm as any);
-  else return true;
-}
-export default isUnique;
