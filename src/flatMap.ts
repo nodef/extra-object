cb6c2c0002b081d9625c5fb15cf87826db27ef71
Subhajit Sahu <wolfram77@gmail.com>
2020-06-10 20:18:42 +0530

    :watermelon: cool fruit +flatMap

---
+++ src/flatMap.ts
@@ -0,0 +1,23 @@
+import is from './is';
+import type {mapFn} from './_types';
+
+function flatMapTo(x: object, fn: mapFn, ths: object, a: object): object {
+  for(var k in x) {
+    if(!x.hasOwnProperty(k)) continue;
+    var v = fn(x[k], k, x);
+    if(is(v)) flatMapTo(v, fn, ths, a);
+    else a[k] = v;
+  }
+  return a;
+}
+
+/**
+ * Flattens nested object, using map function.
+ * @param x a nested object
+ * @param fn map function (v, k, x)
+ * @param ths this argument
+ */
+function flatMap(x: object, fn: mapFn, ths: object=null): object {
+  return flatMapTo(x, fn, ths, {});
+}
+export default flatMap;
