0d8da3288b5ad68ab84c45fe49a06b40c998f400
Subhajit Sahu <wolfram77@gmail.com>
2020-06-10 23:10:48 +0530

    :watermelon: cool fruit +filterAt

--- src/filterAt$.ts
+++ src/filterAt$.ts
@@ -1,4 +1,8 @@
-function filterAt() {
-
+function filterAt$(x: object, ks: string[]): object {
+  for(var k in x) {
+    if(!x.hasOwnProperty(k)) continue;
+    if(!ks.includes(k)) delete x[k];
+  }
+  return x;
 }
-export default filterAt;
+export default filterAt$;
