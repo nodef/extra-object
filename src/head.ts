3a6c00f57ad0efbbc648713d75038536a705316e
Subhajit Sahu <wolfram77@gmail.com>
2020-06-10 12:50:18 +0530

    :watermelon: cool fruit +head

---
+++ src/head.ts
@@ -0,0 +1,11 @@
+/**
+ * Gets first entry.
+ * @param x an object
+ */
+function head(x: object): [string, any] {
+  for(var k in x) {
+    if(!x.hasOwnProperty(k)) continue;
+    return [k, x[k]];
+  }
+}
+export default head;
