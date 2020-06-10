d834df9975b773a71f0e91e81c814465c583843f
Subhajit Sahu <wolfram77@gmail.com>
2020-06-10 14:47:55 +0530

    :watermelon: cool fruit +filter$

---
+++ src/filter$.ts
@@ -0,0 +1,10 @@
+import type {testFn} from './_types';
+
+function filter$(x: object, fn: testFn, ths: object=null): object {
+  for(var k in x) {
+    if(!x.hasOwnProperty(k)) continue;
+    if(!fn.call(ths, x[k], k, x)) delete x[k];
+  }
+  return x;
+}
+export default filter$;
