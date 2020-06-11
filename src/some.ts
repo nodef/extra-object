50e9b0f31e632ea993acb5069208cf25540b0fe9
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 15:18:39 +0530

    :watermelon: cool fruit ;some, every

--- src/some.ts
+++ src/some.ts
@@ -1,5 +1,11 @@
 import type {testFn} from './_types';
 
+/**
+ * Checks if any value satisfies a test.
+ * @param x an object
+ * @param fn test function (v, k, x)
+ * @param ths this argument
+ */
 function some(x: object, fn: testFn, ths: object=null) {
   for(var k in x) {
     if(!x.hasOwnProperty(k)) continue;
