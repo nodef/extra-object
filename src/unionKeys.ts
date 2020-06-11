e84e93f0fe9e03480d3df742ed23183e31cc6479
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 15:33:55 +0530

    :watermelon: cool fruit ;unionKeys, intersectionKeys

--- src/unionKeys.ts
+++ src/unionKeys.ts
@@ -1,10 +1,12 @@
+/**
+ * Gives keys present in any object.
+ * @param xs objects
+ */
 function unionKeys(...xs: object[]): Set<string> {
   var a = new Set<string>();
   for(var x of xs) {
-    for(var k in x) {
-      if(!x.hasOwnProperty(k)) continue;
-      a.add(k);
-    }
+    for(var k in x)
+      if(x.hasOwnProperty(k)) a.add(k);
   }
   return a;
 }
