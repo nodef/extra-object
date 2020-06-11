8aa2e19267abb391e01ef0892953e6a19d646743
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 13:30:04 +0530

    :watermelon: cool fruit ;value

--- src/value.ts
+++ src/value.ts
@@ -1,11 +1,11 @@
+import {value as arrayValue} from 'extra-array';
+
 /**
  * Picks an arbitrary value.
  * @param x an object
  * @param r random seed 0->1
  */
 function value(x: object, r: number=Math.random()): string {
-  var vs = Object.values(x), VS = vs.length;
-  var i  = Math.floor(r*VS);
-  return vs[i];
+  return arrayValue(Object.keys(x), r);
 }
 export default value;
