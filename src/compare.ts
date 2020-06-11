63d30d1f528f85409e90297362b721e39d03102a
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 14:52:12 +0530

    :watermelon: cool fruit ;isEqual

--- src/compare.ts
+++ src/compare.ts
@@ -3,6 +3,14 @@ import cmp from './_cmp';
 import unionKeys from './unionKeys';
 import type {compareFn, mapFn} from './_types';
 
+/**
+ * Compares two objects.
+ * @param x an object
+ * @param y another object
+ * @param fc compare function (a, b)
+ * @param fm map function (v, k, x)
+ * @returns x=y: 0, otherwise: -ve/+ve
+ */
 function compare(x: object, y: object, fc: compareFn=null, fm: mapFn=null): number {
   var fc = fc||cmp, fm = fm||id;
   var ks = unionKeys(x, y);
