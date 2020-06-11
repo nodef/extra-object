0f342c860cbb416244a753105b8006b40e9dc254
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 13:21:18 +0530

    :watermelon: cool fruit ;search

--- src/search.ts
+++ src/search.ts
@@ -1,5 +1,11 @@
 import type {testFn} from './_types';
 
+/**
+ * Finds key of an entry passing a test.
+ * @param x an object
+ * @param fn test function (v, k, x)
+ * @param ths this argument
+ */
 function search(x: object, fn: testFn, ths: object=null): string {
   for(var k in x) {
     if(!x.hasOwnProperty(k)) continue;
--- src/searchAll.ts
+++ src/searchAll.ts
@@ -1,5 +1,11 @@
 import type {testFn} from './_types';
 
+/**
+ * Finds keys of entries passing a test.
+ * @param x an object
+ * @param fn test function (v, k, x)
+ * @param ths this argument
+ */
 function searchAll(x: object, fn: testFn, ths: object=null): string[] {
   var a = [];
   for(var k in x) {
