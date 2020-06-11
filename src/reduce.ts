d347392d968d2dc562ea360d3e07dfa24b4e5e50
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 12:15:19 +0530

    :watermelon: cool fruit ;reduce

--- src/reduce.ts
+++ src/reduce.ts
@@ -1,5 +1,11 @@
 import type {reduceFn} from './_types';
 
+/**
+ * Reduces values to a single value.
+ * @param x an object
+ * @param fn reduce function (acc, v, k, x)
+ * @param acc initial value
+ */
 function reduce<T>(x: object, fn: reduceFn<T>, acc?: T): T {
   var init = arguments.length <= 2;
   for(var k in x) {
