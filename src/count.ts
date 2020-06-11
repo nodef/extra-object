3ac75835feda2984ed736d645b640285f6367a1b
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 14:07:10 +0530

    :watermelon: cool fruit ;count

--- src/count.ts
+++ src/count.ts
@@ -1,5 +1,11 @@
 import type {testFn} from './_types';
 
+/**
+ * Counts values which satisfy a test.
+ * @param x an object
+ * @param fn test function (v, k, x)
+ * @param ths this argument
+ */
 function count(x: object, fn: testFn, ths: object=null): number {
   var a = 0;
   for(var k in x) {
--- src/countAs.ts
+++ src/countAs.ts
@@ -1,6 +1,15 @@
+import id from './_id';
 import type {mapFn} from './_types';
 
+/**
+ * Counts occurrences of values.
+ * @param x an object
+ * @param fn map function (v, k, x)
+ * @param ths this argument
+ * @returns Map {value => count}
+ */
 function countAs(x: object, fn: mapFn, ths: object=null): Map<any, number> {
+  var fn = fn||id;
   var a = new Map();
   for(var k in x) {
     if(!x.hasOwnProperty(k)) continue;
