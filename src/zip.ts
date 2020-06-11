43a5fad62c8dd9b5235306dbf7bfe402aaa9f43e
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 15:07:23 +0530

    :watermelon: cool fruit ;zip

--- src/zip.ts
+++ src/zip.ts
@@ -3,17 +3,20 @@ import unionKeys from './unionKeys';
 import {some} from 'extra-iterable';
 import type {mapFn, tillFn} from './_types';
 
+/**
+ * Combines entries from objects.
+ * @param xs objects
+ * @param fm map function (vs, k, null)
+ * @param ft till function (dones) (some)
+ * @param vd default value
+ */
 function zip(xs: object[], fm: mapFn=null, ft: tillFn=null, vd?: any): object {
   var fm = fm||id, ft = ft||some as tillFn;
   var ks = unionKeys(...xs), a = {};
   for(var k of ks) {
-    var vs = [], ds = [], i = 0;
-    for(var x of xs) {
-      ds[i] = !x.hasOwnProperty(k);
-      vs[i] = ds[i]? vd : x[k];
-      i++;
-    }
+    var ds = xs.map(x => !x.hasOwnProperty(k));
     if(ft(ds)) break;
+    var vs = xs.map(x => !x.hasOwnProperty(k)? vd : x[k]);
     a[k] = fm(vs, k, null);
   }
   return a;
