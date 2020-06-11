2bad85c1b53d60098f67f1b2f4df33d5803c2540
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 11:40:30 +0530

    :watermelon: cool fruit ;swap

--- src/swap$.ts
+++ src/swap$.ts
@@ -1,3 +1,10 @@
+/**
+ * Exchanges two values.
+ * @param x an object (updated)
+ * @param k a key
+ * @param l another key
+ * @returns x
+ */
 function swap$(x: object, k: string, l: string): object {
   var t = x[k]; x[k] = x[l]; x[l] = t;
   return x;
