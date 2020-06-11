e277c658a338866f91094a5ee91259d972937713
Subhajit Sahu <wolfram77@gmail.com>
2020-06-11 16:27:35 +0530

    :watermelon: cool fruit +fromLists

---
+++ src/fromLists.ts
@@ -0,0 +1,12 @@
+/**
+ * Creates object from lisst.
+ * @param ls lists, i.e. [keys, values]
+ */
+function fromLists(ls: [Iterable<string>, Iterable<any>]): object {
+  var [ks, vs] = ls, a = {};
+  var vi = vs[Symbol.iterator]();
+  for(var k of ks)
+    a[k] = vi.next().value;
+  return a;
+}
+export default fromLists;
