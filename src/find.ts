d02cad10b5b6c4298460984ce5d68c5a16984aad
Subhajit Sahu <wolfram77@gmail.com>
2020-06-12 15:18:18 +0530

    :watermelon: cool fruit ;find, findAll

--- src/find.ts
+++ src/find.ts
@@ -1,5 +1,11 @@
 import type {testFn} from './_types';
 
+/**
+ * Finds value of an entry passing a test.
+ * @param x an object
+ * @param fn test function (v, k, x)
+ * @param ths this argument
+ */
 function find(x: object, fn: testFn, ths: object=null): any {
   for(var k in x) {
     if(!x.hasOwnProperty(k)) continue;
--- src/findAll.ts
+++ src/findAll.ts
@@ -1,5 +1,11 @@
 import type {testFn} from './_types';
 
+/**
+ * Finds values of entries passing a test.
+ * @param x an object
+ * @param fn test function (v, k, x)
+ * @param ths this argument
+ */
 function findAll(x: object, fn: testFn, ths: object=null): any[] {
   var a = [];
   for(var k in x) {
